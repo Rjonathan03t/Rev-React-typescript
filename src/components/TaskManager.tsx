be79b620cd8159a96e0fac1074a47b74c4c7c7d9
Jonathan Rajao <hei.tovomihaja@gmail.com>
2024-03-11 11:37:12 +0300

    feat: use the custom hook

--- src/components/TaskManager.tsx
+++ src/components/TaskManager.tsx
@@ -45,41 +45,7 @@ export const useTaskManager=()=>{
 }
 // TODO: create custom hook to manage task state
 export const TaskManager = () => {
-  const [title, setTitle] = useState<string>("");
-  const [searchKeyword, setSearchKeyword] = useState<string>("");
-  const [tasks, setTasks] = useState<Task[]>([]);
-
-  // remove task from list
-  const completeTask = (id: string) :void => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
-  const updateTask = (id :string, taskUpdate: Partial<Task>) :void => {
-    const newTasks = tasks.map((task) => task.id === id ? { ...task, ...taskUpdate } : task);
-    setTasks(newTasks);
-  };
-
-  const addTask = () : void => {
-    if (title.length < 1) {
-      return;
-    }
-
-    const newTask: Task = {
-      // using nanoid to generate unique id
-      id: nanoid(),
-      title,
-    };
-    setTasks((prev) => prev.concat(newTask));
-    setTitle("");
-  };
-
-  const handleSearch = (ev: React.ChangeEvent<HTMLInputElement>) :void => {
-    setSearchKeyword(ev.target.value);
-  };
-
-  const filteredTasks = tasks.filter((task : Task) =>
-    task.title.toLowerCase().includes(searchKeyword.toLowerCase()),
-  );
+  const { title, addTask, completeTask, updateTask, filteredTasks, handleSearch, setTitle } = useTaskManager();
 
   return (
     <div className="container">
@@ -98,7 +64,7 @@ export const TaskManager = () => {
           }}
         />
 
-        <button onClick={addTask}>Add Task</button>
+        <button onClick={() => addTask(title)}>Add Task</button>
       </div>
 
       <ul className="container">
